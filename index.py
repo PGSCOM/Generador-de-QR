4312c2bc58ba5e240ad695cffb97aef24c835891
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-01-11 18:39:18 +0100

    Actualizado para que obtenga desde qr.txt

--- index.py
+++ index.py
@@ -1,5 +1,19 @@
 import qrcode
-obj = qrcode.make( "http://www.youtube.com/c/Byspel?sub_confirmation=1")
-imgQr = open( "qr.png", "wb")
-obj.save( imgQr)
-imgQr.close()
+
+# Abrir el archivo de texto
+with open("qr.txt", "r") as f:
+    lines = f.readlines()
+
+# Recorrer las líneas en el archivo
+for i, line in enumerate(lines):
+    # Crear un objeto QR
+    qr = qrcode.QRCode(version=1, box_size=10, border=0)
+    
+    # Agregar el texto a codificar
+    qr.add_data(line.strip())
+    
+    # Generar la imagen
+    img = qr.make_image()
+    
+    # Guardar la imagen en disco
+    img.save(f"{i + 1}.png")
